c6b095fa5a51c75ff6d4a8e076a40583d87074d1
Welew <will@willbelew.com>
2022-11-27 10:47:43 -0800

    initial commit!

---
+++ README.md
@@ -0,0 +1,4 @@
+MSWN is a body-tracking app that effectively models the health and capabilities of your physical tissues.
+
+It is served on a Flask back-end, and delivered via React on the front-end.
+
---
+++ server_side/runMswn.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask
+
+#from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    # create and config the app
+    app = Flask(__name__, instance_relative_config=True)
+    
+    #CORS(app, origins='*')
+    app.config.from_mapping(
+        #this should be overidden w/ random value when deploying for security
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'mswnapp.sqlite')
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test_config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/health")
+    def health():
+        return "<h1>Health check!</h1>"
+        #what else could I have RUN here that exposes some good stuff??
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
